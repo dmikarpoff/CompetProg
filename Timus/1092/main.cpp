4787df2e332c0dab4d99a212a0517f4e12129410
dmikarpoff <dmikarpoff@gmail.com>
2018-03-17 23:41:36 +0400

    Timus 1092, read of input and main cycle of solution

--- Timus/1092/main.cpp
+++ Timus/1092/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int countPluses(const std::vector<string>& fld)
+{
+    int res = 0;
+    for (const auto& str : fld)
+        for (char c : str)
+            if (c == '+')
+                ++res;
+    return res;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    cin >> n;
+    n = 2 * n + 1;
+    std::vector<string> fld(n);
+    for (int i = 0; i < n; ++i)
+        cin >> fld[i];
+    cout << "There is solution:" << endl;
+    while (countPluses(fld) >= n)
+    {
+    }
     return 0;
 }
