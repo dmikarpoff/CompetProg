8e5a42c4a9b7620d87beb51dc54d1d203db17a52
dmikar <dmitry.karpov@wartsila.com>
2020-11-01 13:49:03 +0300

    added pattern of solution in cpp

---
+++ pattern_main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <queue>
+#include <string>
+
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <cstdint>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
