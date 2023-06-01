72dc97b6145474a22fdbcba3da51098eb24b1958
dmikar <dmitry.karpov@wartsila.com>
2023-06-02 01:58:12 +0300

    Yandex Cup 2021, backend final, task I

--- yandex_cup/backend/2021/final/I/main.cpp
+++ yandex_cup/backend/2021/final/I/main.cpp
@@ -2,161 +2,282 @@
 #include <vector>
 #include <sstream>
 #include <cassert>
+#include <algorithm>
+#include <fstream>
+#include <cassert>
 
 using namespace std;
 
-enum class Combination
+vector<string> tokenize(string line)
 {
-    STRAIT_FLASH = 0,
-    UNKNOWN
-};
-
-struct Hand
-{
-    Combination cmb = Combination::UNKNOWN;
-    int severity = 0;
-};
-
-vector<string> parse_columns(const string& line)
-{
-    size_t lbound = line.find('|');
-    size_t rbound = line.find('|', lbound + 1);
     vector<string> res;
-    while (rbound != string::npos)
+    string cur_token;
+    for (size_t i = 0; i < line.size(); ++i)
     {
-        string t = string(line.begin() + lbound + 1, line.begin() + rbound);
-        stringstream ss;
-        ss.str(t);
-        res.push_back("");
-        ss >> res.back();
-        lbound = rbound;
-        rbound = line.find('|', lbound + 1);
+        if (isalnum(line[i]))
+        {
+          cur_token += line[i];
+        }
+        else if (!cur_token.empty())
+        {
+            res.emplace_back(move(cur_token));
+            cur_token = "";
+        }
+    }
+    if (!cur_token.empty())
+    {
+        res.emplace_back(move(cur_token));
     }
     return res;
 }
 
-pair<int, int> parse_card(const std::string& str)
+enum class Kind
 {
-    int value = 0;
-    int kind = 0;
-    if (str[0] >= '2' && str[0] <= '9')
+    S = 0,
+    H,
+    D,
+    C
+};
+
+Kind get_kind(char c)
+{
+    switch (c)
     {
-        value = str[0] - '2';
-    }
-    else
-    {
-        switch (str[0])
-        {
-        case '1':
-            value = 8;
-            break;
-        case 'J':
-            value = 9;
-            break;
-        case 'Q':
-            value = 10;
-            break;
-        case 'K':
-            value = 11;
-            break;
-        case 'A':
-            value = 12;
-            break;
-        default:
-            assert(false);
-        }
-    }
-    switch (str.back())
-    {
-    case 'H':
-        kind = 0;
-        break;
-    case 'D':
-        kind = 1;
-        break;
     case 'S':
-        kind = 2;
-        break;
+        return Kind::S;
+    case 'H':
+        return Kind::H;
+    case 'D':
+        return Kind::D;
     case 'C':
-        kind = 3;
-        break;
-    default:
-        assert(false);
+        return Kind::C;
     }
-    return make_pair(value, kind);
+    assert(false);
 }
 
-Hand parse_hand(const std::vector<std::vector<char>>& cards)
+struct Card
 {
-    Hand res;
-    for (int i = 0; i < 9; ++i)
+    Card(int v, Kind k) : value{v}, kind{k} {}
+
+    int value;
+    Kind kind;
+};
+
+bool operator<(Card lhs, Card rhs)
+{
+    return lhs.value < rhs.value;
+}
+
+bool is_straight_flush(const std::vector<Card>& hand)
+{
+    for (size_t i = 0; i < 4; ++i)
     {
-        for (int h = 0; h < 4; ++h)
+        if (hand[i].value + 1 != hand[i + 1].value || hand[i].kind != hand[i + 1].kind)
         {
-            bool str_flush = true;
-            for (int k = 0; k < 5; ++k)
+            return false;
+        }
+    }
+    return true;
+}
+
+pair<int, int> get_hand_strength(const vector<Card>& hand)
+{
+    int cnt[15] = {0};
+    for (size_t i = 0; i < 5; ++i)
+    {
+        cnt[hand[i].value]++;
+    }
+    for (auto x : cnt)
+    {
+        cerr << x << " ";
+    }
+    cerr << endl;
+    // straight flush
+    if (is_straight_flush(hand))
+    {
+        cerr << "Straight flush" << endl;
+        return {100, hand[4].value};
+    }
+    {
+        // four of a kind
+        auto i = find(begin(cnt), end(cnt), 4);
+        if (i != end(cnt))
+        {
+            cerr << "Four of a kind" << endl;
+            return {99, i - begin(cnt)};
+        }
+    }
+    {
+        // full house
+        auto a = find(begin(cnt), end(cnt), 3);
+        auto b = find(begin(cnt), end(cnt), 2);
+        if (a != end(cnt) && b != end(cnt))
+        {
+            cerr << "Full house" << endl;
+            return {98, a - begin(cnt)};
+        }
+    }
+    {
+        // flush
+        bool l = true;
+        for (auto c: hand)
+        {
+            if (c.kind != hand[0].kind)
             {
-                if (cards[i + k][h] == 0)
-                {
-                    str_flush = false;
-                    break;
-                }
+                l = false;
             }
-            if (str_flush)
+        }
+        if (l)
+        {
+            cerr << "Flush" << endl;
+            int s = 0;
+            for (size_t i = 0; i < 5; ++i)
             {
-                res.cmb = Combination::STRAIT_FLASH;
-                res.severity = max(i, res.severity);
+                s += s * 15 + hand[4 - i].value;
+            }
+            return {97, s};
+        }
+    }
+    {
+        bool straight = true;
+        for (size_t i = 0; i < 4; ++i)
+            if (hand[i].value + 1 != hand[i + 1].value)
+        {
+            straight = false;
+        }
+        if (straight)
+        {
+            cerr << "Straight" << endl;
+            return {96, hand[4].value};
+        }
+    }
+    {
+        auto i = find(begin(cnt), end(cnt), 3);
+        if (i != end(cnt))
+        {
+            cerr << "Set" << endl;
+            return {95, i - begin(cnt)};
+        }
+    }
+    {
+        auto i = find(begin(cnt), end(cnt), 2);
+        if (i != end(cnt))
+        {
+            auto j = find(i + 1, end(cnt), 2);
+            if (j != end(cnt))
+            {
+                cerr << "Two pairs" << endl;
+                return {92, (j - begin(cnt)) * 100 + (i - begin(cnt))};
+            }
+            else
+            {
+                cerr << "Pair" << endl;
+                return {90, i - begin(cnt)};
             }
         }
     }
-    if (res.cmb != Combination::UNKNOWN)
-    {
-        return res;
-    }
-    vector<char> cnt(13, 0);
-    for (int i = 0; i < 13; ++i)
-    {
-        for (int j = 0; j < 4; ++j)
-            cnt[i] += cards[i][j];
-    }
-    return res;
+    return {1, hand[4].value};
 }
 
 int main()
 {
+    ifstream in("input.txt");
     string line;
-    while (cin)
+    vector<Card> columns;
+    columns.reserve(52);
+    pair<int, int> best_hand{0, 0};
+    vector<string> winners;
+    while (getline(in, line, '\n'))
     {
-        string splitter, header;
-        getline(cin, splitter);
-        getline(cin, header);
-        auto columns = parse_columns(header);
-        vector<pair<int, int>> column_values;
-        for (size_t i = 1; i < columns.size(); ++i)
+        line.erase(remove_if(line.begin(), line.end(),
+                             [](char c) {return c == ' ' || c == '\t';}),
+                   line.end());
+        if (line.empty() && !winners.empty())
         {
-            column_values.push_back(parse_card(columns[i]));
-        }
-        for (;;)
-        {
-            getline(cin, splitter);
-            string row;
-            getline(cin, row);
-            if (row.empty())
+            sort(winners.begin(), winners.end());
+            for (const auto& w: winners)
             {
-                break;
+                cout << w << " ";
             }
-            auto split_row = parse_columns(row);
-            auto surname = split_row[0];
-            vector<vector<char>> cur_cards(13, vector<char>(4, 0));
-            cur_cards.resize(13, vector<char>(4, 0));
-            for (size_t i = 1; i < split_row.size(); ++i)
+            cout << endl;
+            winners.clear();
+            best_hand = {0, 0};
+            columns.clear();
+            columns.reserve(52);
+            continue;
+        }
+        auto tokens = tokenize(move(line));
+        if (tokens.empty())
+        {
+            continue;
+        }
+        assert(tokens.size() == 53);
+        if (columns.empty())
+        {
+            for (const auto& t : tokens)
             {
-                if (split_row[i] == "1")
+                if (t.size() < 2 || t.size() > 3)
                 {
-                    cur_cards[column_values[i].first][column_values[i].second] = 1;
+                    continue;
                 }
+                if (t.size() == 3)
+                {
+                    columns.emplace_back(10, get_kind(t[2]));
+                    continue;
+                }
+                int val = 0;
+                switch (t[0])
+                {
+                case 'J':
+                    val = 11;
+                    break;
+                case 'Q':
+                    val = 12;
+                    break;
+                case 'K':
+                    val = 13;
+                    break;
+                case 'A':
+                    val = 14;
+                    break;
+                default:
+                    val = t[0] - '0';
+                }
+                assert(val > 0 && val < 15);
+                columns.emplace_back(val, get_kind(t[1]));
             }
+            continue;
         }
+        assert(columns.size() == 52);
+        std::vector<Card> hand;
+        auto name = tokens[0];
+        for (size_t i = 1; i < tokens.size(); ++i)
+            if (tokens[i] == "1")
+        {
+            hand.push_back(columns[i - 1]);
+        }
+        assert(hand.size() == 5);
+        sort(hand.begin(), hand.end());
+        pair<int, int> s = get_hand_strength(hand);
+        if (s > best_hand)
+        {
+            winners.clear();
+            best_hand = s;
+        }
+
+        if (s >= best_hand)
+        {
+            winners.push_back(name);
+        }
+    }
+    if (!winners.empty())
+    {
+        sort(winners.begin(), winners.end());
+        for (const auto& w: winners)
+        {
+            cout << w << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
